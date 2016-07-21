1251fc01df6cf323019f10936e466af859447ef7
gary721400 <lyf721400@126.com>
2016-07-21 11:03:20 +0800

    Initial commit

---
+++ app/src/main/java/com/afaya/recordpen/Choice_Ble.java
@@ -0,0 +1,13 @@
+package com.afaya.recordpen;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Choice_Ble extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_choice__ble);
+    }
+}
